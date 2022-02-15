ec33106dc2fdb7b0c0ee85c55d8be7c1ba05a8a0
Griffin Warner <griffinwarner77@gmail.com>
2022-02-15 15:52:19 -0500

    Fix

--- homework/AssignmentTwo/AssignmentTwo/src/ThirtySix.java
+++ homework/AssignmentTwo/AssignmentTwo/src/ThirtySix.java
@@ -4,8 +4,6 @@ import java.util.Date;
 
 public class ThirtySix {
     public static void main(String[] args) {
-        // Fuck the textbook for this date problem, dates are objectively the most fucking annoying thing to work with and
-        // you'll never create your own method - just use other libraries. Also all the useful java stuff is deprecated because 
-        // there are FAR BETTER OPTIONS
+        
     }
 }
