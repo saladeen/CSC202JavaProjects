ec6ae1009f7125c503f54aa5c6ce99075e5442f1
Griffin Warner <griffinwarner77@gmail.com>
2022-02-22 13:45:07 -0500

    syncing

--- homework/AssignmentThree/AssignmentThree/src/Parallelogram.java
+++ homework/AssignmentThree/AssignmentThree/src/Parallelogram.java
@@ -1,3 +1,20 @@
-public class Parallelogram {
+public class Parallelogram implements FigureInterface {
+    double height, base, angleInRad;
+
+    public Parallelogram(double base, double height, double angleInRad) {
+
+    }
+
+    @Override
+    public double perimeter() {
+        // calculate side lengths
+        // sin(angle)
+    }
+
+    @Override
+    public double area() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
     
 }
